512429d723a02d98f4c745ae3e51ef45470e272d
Moufid17 <moufid.mtr@gmail.com>
2022-05-01 17:34:22 +0200

    Define repository content(username,fname,lname,email, pwd,cpwd) attributes.

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -4,6 +4,11 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+// use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +17,66 @@ class RegisterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('email')
-            ->add('password')
+            ->add('username',TextType::class, 
+                [
+                    'label' => 'Username',
+                    'required' => true,
+                    'attr'  => [
+                        'placeholder' => 'JDeo',
+                    ]
+                ]
+            )
+            ->add('firstname',TextType::class, 
+                [
+                    'label' => 'Firstname',
+                    'required' => true,
+                    'attr' => [
+                        'placeholder' => 'John',
+                    ]    
+                ]
+            )
+            ->add('lastname',TextType::class, 
+                [
+                    'label' => 'Lastname',
+                    'required' => true,
+                    'attr' => [
+                        'placeholder' => 'Deo',
+                    ]
+                ]
+            )
+            ->add('email', EmailType::class,
+                [
+                    'label' =>'Email',
+                    'required' => true,
+                    'attr' => [
+                        'placeholder' => 'john.deo@xyz.com',
+                    ]    
+                ]
+            )
+            ->add('password',PasswordType::class,[
+                    'label' => 'Password',
+                    'required' => true,
+                    'attr' => [
+                        'placeholder' => 'Password'
+                    ]
+                ]
+            )
+            ->add('cpassword',PasswordType::class,[
+                    'label' => 'Confirm Password',
+                    'required' => true,
+                    'mapped' => false,
+                    'attr' => [
+                        'placeholder' => 'Confirm Password'
+                    ]
+                ]
+            )
+            ->add('agreeTerms',CheckboxType::class,[
+                    'label' => 'Accept our user terms',
+                    'required' => true,
+                    'mapped' => false,
+                ]
+            )
+
         ;
     }
 

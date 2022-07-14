564767fc5985df39d9a80f66527d531e4d7a3b1a
TORKIAHALA <halatorkia28@gmail.com>
2022-07-14 10:59:58 +0200

    pagination

--- src/Controller/Front/ProductController.php
+++ src/Controller/Front/ProductController.php
@@ -9,35 +9,69 @@ use App\Form\SearchType;
 use App\Security\ProductVoter;
 use App\Repository\ProductRepository;
 use App\Services\Search;
+use ContainerFRB68Wn\PaginatorInterface_82dac15;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
 
 class ProductController extends AbstractController
 {
     #[Route('/produits', name: 'app_product', methods: ['GET', 'POST'])]
-    public function index(Request $request,EntityManagerInterface $entityManager): Response
+    public function index(Request $request,EntityManagerInterface $entityManager, PaginatorInterface  $paginator): Response
     {
         $search = new Search();
         $form = $this->createForm(SearchType::class, $search);
         $form->handleRequest($request);
+    
 
         $product = $entityManager->getRepository(Product::class)->findAll();
-
+    
         if($form->isSubmitted() && $form->isValid()){
             $product = $entityManager->getRepository(Product::class)->findBySearch($search);
-        }
+        };
+
+        $product =$paginator->paginate(
+            $product,
+            $request->query->getInt("page",1),1
+        );
+        
+        // $product =$paginator->paginate(
+        //     $entityManager->getRepository(Product::class)->findBySearch($search),
+        //     $request->query->getInt("page",1),12
+        // );
 
         return $this->render('front/product/index.html.twig', [
             'products' => $product,
             'form' => $form->createView(),
         ]);
     }
+    /*#[Route('/produits', name: 'app_product', methods: ['GET', 'POST'])]
+    public function index(Request $request,PaginatorInterface_82dac15 $paginator): Response
+    {
+        $search = new Search();
+        $form = $this->createForm(SearchType::class, $search);
+        $form->handleRequest($request);
+
+        $product =$paginator->paginate(
+            $this->repository->findBySearch(),
+            $request->query->getInt(key:"page",default:1),limit:12
+        
+        );
+
+       
+
+        return $this->render('front/product/index.html.twig', [
+            'products' => $product,
+            'form' => $form->createView(),
+        ]);
+    }*/
 
     #[Route('/produits/creer', name: 'app_new_product', methods: ['GET','POST'])]
     public function new(Request $request, EntityManagerInterface $em): Response

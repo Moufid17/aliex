9d049765fc6af9c4090cfdf60654d70cb29e4c4d
TORKIAHALA <halatorkia28@gmail.com>
2022-07-15 10:15:25 +0200

    CategoryFixture

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\DataFixtures;
-
+use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +9,14 @@ class CategoryFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+    
+         $faker = \Faker\Factory::create();
+         for ($i = 0; $i < 10; $i++) {
+              $category = new Category();
+              $category->setName($faker->name);
+              $manager->persist($category);
+
+         }
 
         $manager->flush();
     }
